e542bbfd2fb6023733ce14331918f4a25c018306
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2025-10-29 14:53:24 +0500

    modified vite-config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:  '/Akfa-Medline-Social/',
   optimizeDeps: {
     exclude: ["react-redux", "@reduxjs/toolkit"],
   },
