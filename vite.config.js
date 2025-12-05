68931e98b26034d774e8ce1cb7b0da24b5520176
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2025-12-05 17:12:24 +0500

    preloading css to improve FCP and LCP

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-
+import preload from "vite-plugin-preload";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), preload(), tailwindcss()],
   base: "/Akfa-Medline-Social/",
 });
